934d195c17c4e0fb2b3567cb6eba202ce6243072
msoodb <masoud.bolhassani@gmail.com>
2023-10-01 13:50:15 +0300

    run wc -l

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+    "os/exec"
 	"io/ioutil"
 	"log"
 )
@@ -14,5 +15,15 @@ func main() {
 
 	for _, f := range files {
 		fmt.Println(f.Name())
+        cmd := exec.Command("wc", "-l", f.Name())
+        stdout, err := cmd.Output()
+
+        if err != nil {
+            fmt.Println(err.Error())
+            return
+        }
+
+        // Print the output
+        fmt.Println(string(stdout))
 	}
 }
