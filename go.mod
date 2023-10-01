626012480cfcd1decf03fa188261203be7d889ac
msoodb <masoud.bolhassani@gmail.com>
2023-10-01 13:26:56 +0300

    list files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/msoodb/wcall
+
+go 1.21.1
--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 )
 
 func main() {
-	fmt.Println("hello world")
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		fmt.Println(f.Name())
+	}
 }
